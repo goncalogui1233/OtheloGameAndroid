fc999b77ceb8c9c1b7c8b1ac5a3c2f5d615e3a6f
goncaloguilherme <goncalo.guilherme@goodbarber.com>
2021-12-07 20:37:02 +0000

    Updated camera api + profile

--- app/src/main/java/com/example/otello/ProfileActivity.kt
+++ app/src/main/java/com/example/otello/ProfileActivity.kt
@@ -1,22 +1,48 @@
 package com.example.otello
 
-import android.content.Intent
-import android.graphics.Bitmap
+import android.content.Context
+import android.content.DialogInterface
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.otello.photo.PhotoActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.content.edit
+import com.example.otello.photo.PhotoDialog2
 import kotlinx.android.synthetic.main.activity_profile.*
 import java.io.File
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity(), DialogInterface.OnDismissListener {
+
+    var playerName : String = ""
+    var photoName : String = ""
+    var menu : Menu? = null
+    var editMode = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        btnCamera.setOnClickListener {
-            startActivity(Intent(this, PhotoActivity::class.java))
+        readFromSharedPreferences()
+
+        nome_jogador.setText(playerName)
+
+        val imgPath = File(applicationInfo.dataDir + "/pic.jpg")
+        if(imgPath.exists()){
+            val b = BitmapFactory.decodeFile(imgPath.absolutePath)
+            player_image.setImageBitmap(b)
+            player_image.rotation = 90f
         }
+
+        btnCamera.setOnClickListener {
+            val fr = PhotoDialog2()
+            fr.show(supportFragmentManager, "JJJ")
+        }
+    }
+
+    override fun onDismiss(dialogInt: DialogInterface?) {
 
         val imgPath = File(applicationInfo.dataDir + "/pic.jpg")
 
@@ -25,5 +51,56 @@ class ProfileActivity : AppCompatActivity() {
             player_image.setImageBitmap(b)
             player_image.rotation = 90f
         }
+    }
+
+    private fun readFromSharedPreferences() {
+        val pref = getSharedPreferences("ProfileInfo", Context.MODE_PRIVATE)
+
+        playerName = pref.getString("PLAYER_NAME", "").toString()
+        photoName = pref.getString("PHOTO_NAME", "").toString()
+    }
+
+    private fun saveOnSharedPreferences() {
+        getSharedPreferences("ProfileInfo", Context.MODE_PRIVATE).edit {
+            putString("PLAYER_NAME", playerName)
+            putString("PHOTO_NAME", photoName)
+            commit()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        this.menu = menu
+        menuInflater.inflate(R.menu.profile_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.profileEdit){
+            if(editMode){
+                editMode = false
+                deactivateEdit()
+                menu?.getItem(0)?.icon = ContextCompat.getDrawable(this, R.drawable.ic_edit)
+            }
+            else {
+                editMode = true
+                activateEdit()
+                menu?.getItem(0)?.icon = ContextCompat.getDrawable(this, R.drawable.ic_save)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    private fun activateEdit() {
+        nome_jogador.isEnabled = true
+        btnCamera.visibility = View.VISIBLE
+    }
+
+    private fun deactivateEdit() {
+        nome_jogador.isEnabled = false
+
+        btnCamera.visibility = View.GONE
+        playerName = nome_jogador.text.toString()
+        saveOnSharedPreferences()
     }
 }
---
+++ app/src/main/java/com/example/otello/photo/PhotoDialog2.kt
@@ -0,0 +1,96 @@
+package com.example.otello.photo
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
+import com.example.otello.R
+import kotlinx.android.synthetic.main.photo_dialog.*
+import kotlinx.android.synthetic.main.photo_dialog.view.*
+import java.io.File
+import java.lang.Exception
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class PhotoDialog2 : DialogFragment() {
+
+    private lateinit var cameraExecutor: ExecutorService
+    private var directory : String = ""
+    private var imageCapture : ImageCapture? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.photo_dialog, container, true)
+
+        directory = requireActivity().applicationContext.filesDir.toString()
+
+        //permission
+
+        view.btnTakePhoto.setOnClickListener {
+            takePhoto()
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        startCamera()
+
+        return view
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+
+        val file = File(directory + "/photo.jpg")
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(file).build()
+
+        imageCapture.takePicture(outputOptions, ContextCompat.getMainExecutor(requireContext()),
+        object : ImageCapture.OnImageSavedCallback {
+            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                Toast.makeText(requireContext(), "Photo Saved - ${directory + "photo.jpg"}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+                Toast.makeText(requireContext(), "Photo Not Saved", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(cameraPreview.surfaceProvider)
+            }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+            try{
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            }catch (e : Exception) {
+                Log.e("TAG", "Use case binding failed")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+}
